f569520824b2856c6f86aefb556681fe6e028383
maryamabdul-aziz <maryam.d39c@gmail.com>
2023-11-28 13:32:46 -0800

    update some stuff

--- assets/js/platformer/Player.js
+++ assets/js/platformer/Player.js
@@ -1,47 +1,35 @@
 import GameEnv from './GameEnv.js';
 import Character from './Character.js';
 
+const PlayerAnimation = {
+    // Sprite properties
+    scale: 0.33,
+    width: 256,
+    height: 256,
+    w: { row: 12, frames: 15 }, // jump key
+	a: { row: 3, frames: 7, idleFrame: { column: 7, frames: 0 } }, // Walk left key
+    s: { }, // no action
+	d: { row: 2, frames: 7, idleFrame: { column: 7, frames: 0 } }, // Walk right key
+}
+
 export class Player extends Character{
     // constructors sets up Character object 
-    constructor(canvas, image, speedRatio, playerData){
+    constructor(canvas, image, speedRatio){
         super(canvas, 
             image, 
             speedRatio,
-            playerData.width, 
-            playerData.height, 
+            PlayerAnimation.width, 
+            PlayerAnimation.height, 
+            PlayerAnimation.scale
         );
-        // Player Data is required for Animations
-        this.playerData = playerData;
-
-        // Player control data
-        this.pressedKeys = {};
-        this.movement = {left: true, right: true, down: true};
+        this.sceneStarted = false;
         this.isIdle = true;
-        this.stashKey = "d"; // initial key
-
-        // Store a reference to the event listener function
-        this.keydownListener = this.handleKeyDown.bind(this);
-        this.keyupListener = this.handleKeyUp.bind(this);
-
-        // Add event listeners
-        document.addEventListener('keydown', this.keydownListener);
-        document.addEventListener('keyup', this.keyupListener);
-
-        GameEnv.player = this;
+        this.yVelocity = 0;
+        this.stashFrame = PlayerAnimation.d;
+        this.pressedDirections = {};
     }
 
-    setAnimation(key) {
-        // animation comes from playerData
-        var animation = this.playerData[key]
-        // direction setup
-        if (key === "a") {
-            this.stashKey = key;
-            this.playerData.w = this.playerData.wa;
-        } else if (key === "d") {
-            this.stashKey = key;
-            this.playerData.w = this.playerData.wd;
-        }
-        // set frame and idle frame
+    setAnimation(animation) {
         this.setFrameY(animation.row);
         this.setMaxFrame(animation.frames);
         if (this.isIdle && animation.idleFrame) {
@@ -53,128 +41,93 @@ export class Player extends Character{
     // check for matching animation
     isAnimation(key) {
         var result = false;
-        if (key in this.pressedKeys) {
-            result = !this.isIdle;
+        for (let direction in this.pressedDirections) {
+            if (this.pressedDirections[direction] === key.row) {
+                result = !this.isIdle;
+                break; // Exit the loop if there's a match
+            }
         }
-        
+        //result = (result && !this.isIdle);
+        if (result) {
+                this.stashFrame = key;
+        }
         return result;
     }
 
     // check for gravity based animation
     isGravityAnimation(key) {
         var result = false;
-    
-        // verify key is in active animations
-        if (key in this.pressedKeys) {
-            result = (!this.isIdle && this.bottom <= this.y);
+        for (let direction in this.pressedDirections) {
+            if (this.pressedDirections[direction] === key.row) {
+                result = (!this.isIdle && GameEnv.bottom <= this.y);
+                break; // Exit the loop if there's a match
+            }
         }
-
-        // scene for on top of tube animation
-        if (!this.movement.down) {
-            this.gravityEnabled = false;
-            // Pause for two seconds
-            setTimeout(() => {   // animation in tube
-                // This code will be executed after the two-second delay
-                this.movement.down = true;
-                this.gravityEnabled = true;
-                setTimeout(() => { // move to end of game detection
-                    this.x = GameEnv.innerWidth + 1;
-                }, 1000);
-            }, 2000);
-        }
-    
-        // make sure jump has ssome velocity
+        //result = (result && !this.isIdle && GameEnv.bottom <= this.y);
+        //var result = (this.frameY === key.row && !this.isIdle && GameEnv.bottom <= this.y);
         if (result) {
-            // Adjust horizontal position during the jump
-            const horizontalJumpFactor = 0.1; // Adjust this factor as needed
-            this.x += this.speed * horizontalJumpFactor;  
+            return true;
         }
-    
-        // return to directional animation (direction?)
-        if (this.bottom <= this.y) {
-            this.setAnimation(this.stashKey);
+        if (GameEnv.bottom <= this.y) {
+            this.setAnimation(this.stashFrame);
         }
-    
-        return result;
+        return false;
     }
-    
 
-    // Player updates
+    // Player perform a unique update
     update() {
-        if (this.isAnimation("a")) {
-            if (this.movement.left) this.x -= this.speed;  // Move to left
+        if (this.isAnimation(PlayerAnimation.a)) {
+            this.x -= this.speed;  // Move to left
         }
-        if (this.isAnimation("d")) {
-            if (this.movement.right) this.x += this.speed;  // Move to right
+        if (this.isAnimation(PlayerAnimation.d)) {
+            this.x += this.speed;  // Move to right
         }
-        if (this.isGravityAnimation("w")) {
-            if (this.movement.down) this.y -= (this.bottom * .33);  // jump 33% higher than bottom
+        if (this.isGravityAnimation(PlayerAnimation.w)) {
+            this.y -= (GameEnv.bottom * .33);  // jump 33% higher than floor
         } 
 
         // Perform super update actions
         super.update();
     }
 
-    // Player action on collisions
-    collisionAction() {
-        if (this.collisionData.touchPoints.other.id === "tube") {
-            // Collision with the left side of the Tube
-            if (this.collisionData.touchPoints.other.left) {
-                this.movement.right = false;
-            }
-            // Collision with the right side of the Tube
-            if (this.collisionData.touchPoints.other.right) {
-                this.movement.left = false;
-            }
-            // Collision with the top of the player
-            if (this.collisionData.touchPoints.other.ontop) {
-                this.movement.down = false;
-                this.x = this.collisionData.touchPoints.other.x;
-            }
-        } else {
-            // Reset movement flags if not colliding with a tube
-            this.movement.left = true;
-            this.movement.right = true;
-            this.movement.down = true;
-        }
-    }
-    
-    // Event listener key down
-    handleKeyDown(event) {
-        if (this.playerData.hasOwnProperty(event.key)) {
-            const key = event.key;
-            if (!(event.key in this.pressedKeys)) {
-                this.pressedKeys[event.key] = this.playerData[key];
-                this.setAnimation(key);
-                // player active
-                this.isIdle = false;
-            }
-        }
-    }
-
-    // Event listener key up
-    handleKeyUp(event) {
-        if (this.playerData.hasOwnProperty(event.key)) {
-            const key = event.key;
-            if (event.key in this.pressedKeys) {
-                delete this.pressedKeys[event.key];
-            }
-            this.setAnimation(key);  
-            // player idle
-            this.isIdle = true;     
-        }
-    }
-
-    // Override destroy() method from GameObject to remove event listeners
-    destroy() {
-        // Remove event listeners
-        document.removeEventListener('keydown', this.keydownListener);
-        document.removeEventListener('keyup', this.keyupListener);
-
-        // Call the parent class's destroy method
-        super.destroy();
-    }
 }
 
+// Can add specific initialization parameters for the player here
+// In this case the player is following the default character initialization
+export function initPlayer(canvas, image, gameSpeed, speedRatio){
+    // Create the Player
+    var player = new Player(canvas, image, gameSpeed, speedRatio);
+
+    /* Player Control 
+    * changes FrameY value (selected row in sprite)
+    * change MaxFrame according to value in selected animation
+    */
+    document.addEventListener('keydown', function (event) {
+        if (PlayerAnimation.hasOwnProperty(event.key)) {
+            // Set variables based on the key that is pressed
+            const key = event.key;
+            if (!(event.key in player.pressedDirections)){
+                player.pressedDirections[event.key] = PlayerAnimation[key].row;
+            }
+            player.isIdle = false;
+            player.setAnimation(PlayerAnimation[key]);
+        }
+    });
+
+    document.addEventListener('keyup', function (event) {
+        if (PlayerAnimation.hasOwnProperty(event.key)) {
+            // If no button is pressed then idle
+            const key = event.key;
+            if (event.key in player.pressedDirections){
+                delete player.pressedDirections[event.key];
+            }
+            player.isIdle = true;
+            player.setAnimation(PlayerAnimation[key]);
+        }
+    });
+
+    // Player Object
+    return player;
+}
 
 export default Player;
